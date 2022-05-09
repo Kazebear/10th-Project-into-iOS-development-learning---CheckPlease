08c653c54de761ab4b16132afa91799d64c18a1f
Mara <kazee.bear@mail.com>
2022-05-10 01:26:15 +0300

    Initial Commit

---
+++ CheckPlease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckPlease
+//
+//  Created by Duca Mariangela on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
